48967ba9e8f013a9d0e62791c88c9634e4c0771d
Soheil Novinfard <novinfard@gmail.com>
2018-01-26 20:22:35 +0000

    Add first test

---
+++ UnitTestingWithXCTest/PlayData.swift
@@ -0,0 +1,13 @@
+//
+//  PlayData.swift
+//  UnitTestingWithXCTest
+//
+//  Created by Soheil on 26/01/2018.
+//  Copyright © 2018 Soheil Novinfard. All rights reserved.
+//
+
+import Foundation
+
+class PlayData {
+	var allWords = [String]()
+}
--- UnitTestingWithXCTestTests/UnitTestingWithXCTestTests.swift
+++ UnitTestingWithXCTestTests/UnitTestingWithXCTestTests.swift
@@ -20,17 +20,10 @@ class UnitTestingWithXCTestTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
+	
+	
+	func testAllWordsLoaded() {
+		let playdata = PlayData()
+		XCTAssertEqual(playdata.allWords.count, 0, "allWords must be 0")
+	}
 }
